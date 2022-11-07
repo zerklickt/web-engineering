ead0283fb590a94e96df616ed1383c7972946989
Zerklickt <zerklickt@gmail.com>
2022-11-07 17:46:11 +0100

    cleaned up style.css pt. 1, extracted theme colors

--- Templates/Headerfix.js
+++
@@ -1,8 +0,0 @@
-<script>
-document.getElementsByTagName("BODY")[0].onload = function() {adjustHeader()};
-document.getElementsByTagName("BODY")[0].onresize = function() {adjustHeader()};
-function adjustHeader() {
-  var offset = document.getElementById('headerfix').offsetHeight;
-  document.getElementById('scrollpart').style.top = offset +"px";
-}
-</script>
